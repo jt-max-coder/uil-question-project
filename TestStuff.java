a97cf0ae3724c1a3ca8a97a0db6bc3fda7140131
jt-max-coder <jte321jr@gmail.com>
2025-08-20 09:20:20 -0500

    Update TestStuff.java

--- TestStuff.java
+++ TestStuff.java
@@ -1,11 +1,15 @@
 Public class TestStuff{
     private AtharvaPalendrome testClass;
     public TestStuff(){
-        System.out.println(testClass.onlyPalendromes(""))
-        System.out.println(testClass.onlyPalendromes(""))
-        System.out.println(testClass.onlyPalendromes(""))
-        System.out.println(testClass.onlyPalendromes(""))
-        System.out.println(testClass.onlyPalendromes(""))
-        System.out.println(testClass.onlyPalendromes(""))
+        testClass = new AtharvaPalendrome();
+    }
+
+    public static void main(String[] args){
+        System.out.println(testClass.onlyPalendromes("Racecar Racecar what??"))
+        System.out.println(testClass.onlyPalendromes("Racecar!!!!"))
+        System.out.println(testClass.onlyPalendromes("Racecar."))
+        System.out.println(testClass.onlyPalendromes("Racecar?? Racecar"))
+        System.out.println(testClass.onlyPalendromes("tit for tat! yay!!!"))
+        System.out.println(testClass.onlyPalendromes("Racecar what is the Racecar doing here bob??"))
     }
 }
