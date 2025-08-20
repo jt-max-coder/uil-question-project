f591abf7a165ad844d47937e9313858c65d76a03
jt-max-coder <jte321jr@gmail.com>
2025-08-20 09:29:28 -0500

    cs

--- AtharvaPalendrome.java
+++ AtharvaPalendrome.java
@@ -1,29 +1,30 @@
-Public class AtharvaPalendrome{
-	Public KingArthur(){
+import java.util.*;
+public Class AtharvaPalendrome {
+	Public AtharvaPalendrome(){
 
     }
 
-    Public boolean isPalendrome(String input){
-	    String before = input.remove(“ “);
-	    String after = “”;
+    public boolean isPalendrome(String input){
+	    String before = input;
+	    String after = "";
 	    for(int i = before.length()-1; i >= 0; i--){
-		    after += before.getIndex(i);
+		    after += before;
 	    }
 	    if(before.equals(after))
-		    Return true;
-	    Return false
+		    return true;
+	    return false;
     }
 
-    Public String onlyPalendromes(String input){
-	    String[] words = String.split(“ “);
-        string retString = "";
+    public String onlyPalendromes(String input){
+	    String[] words = String.split(" ");
+        String retString = "";
         for(int i = 0; i < words.length; i++){
             wordsTest = words[i];
             if(isPalendrome(wordsTest.remove("\\p{Punct}")))     
-                retString += words[i] + " "
+                retString += words[i] + " ";
         }
         retString.remove(retString.length-1);
-        return retString
+        return retString;
     }
 }
 	
--- TestStuff.java
+++ TestStuff.java
@@ -1,15 +1,11 @@
 Public class TestStuff{
-    private AtharvaPalendrome testClass;
-    public TestStuff(){
-        testClass = new AtharvaPalendrome();
-    }
-
     public static void main(String[] args){
-        System.out.println(testClass.onlyPalendromes("Racecar Racecar what??"))
-        System.out.println(testClass.onlyPalendromes("Racecar!!!!"))
-        System.out.println(testClass.onlyPalendromes("Racecar."))
-        System.out.println(testClass.onlyPalendromes("Racecar?? Racecar"))
-        System.out.println(testClass.onlyPalendromes("tit for tat! yay!!!"))
-        System.out.println(testClass.onlyPalendromes("Racecar what is the Racecar doing here bob??"))
+        AtharvaPalendrome testClass = new AtharvaPalendrome();
+        System.out.println(testClass.onlyPalendromes("Racecar Racecar what??"));
+        System.out.println(testClass.onlyPalendromes("Racecar!!!!"));
+        System.out.println(testClass.onlyPalendromes("Racecar."));
+        System.out.println(testClass.onlyPalendromes("Racecar?? Racecar"));
+        System.out.println(testClass.onlyPalendromes("tit for tat! yay!!!"));
+        System.out.println(testClass.onlyPalendromes("Racecar what is the Racecar doing here bob??"));
     }
 }
