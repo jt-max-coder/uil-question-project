98bc5f4f91713d39b60a7617be155ecebd3471fa
jt-max-coder <jte321jr@gmail.com>
2025-08-20 09:16:44 -0500

    made stuff

---
+++ TestStuff.java
@@ -0,0 +1,11 @@
+Public class TestStuff{
+    private AtharvaPalendrome testClass;
+    public TestStuff(){
+        System.out.println(testClass.onlyPalendromes(""))
+        System.out.println(testClass.onlyPalendromes(""))
+        System.out.println(testClass.onlyPalendromes(""))
+        System.out.println(testClass.onlyPalendromes(""))
+        System.out.println(testClass.onlyPalendromes(""))
+        System.out.println(testClass.onlyPalendromes(""))
+    }
+}
